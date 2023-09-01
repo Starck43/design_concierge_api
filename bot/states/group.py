e273f1f47dfb0d947a1877941a9d70c750a462e8
Starck43 <saloon.as@gmail.com>
2023-09-01 21:38:40 +0300

    add comments

--- bot/states/group.py
+++ bot/states/group.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class Group(Enum):
-	DESIGNER: int = 0
-	OUTSOURCER: int = 1
-	SUPPLIER: int = 2
+	DESIGNER: int = 0  # Если пользователь и дизайнер и аутсорсер
+	OUTSOURCER: int = 1  # Если пользователь только аутсорсер
+	SUPPLIER: int = 2  # Если пользователь только поставщик
 	UNCATEGORIZED: int = 3
 
 	@staticmethod
