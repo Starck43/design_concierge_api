8a8f4f8e343bd49dc72d906e5e95544565ccde24
Starck43 <saloon.as@gmail.com>
2023-12-29 15:37:28 +0300

    add django setup

--- crm/wsgi.py
+++ crm/wsgi.py
@@ -1,8 +1,8 @@
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm.settings")
+django.setup()
 
 application = get_wsgi_application()
